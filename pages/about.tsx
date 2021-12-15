62863ebada562aeb7e9d48f151e4fb2f021ae01a
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2021-12-15 19:00:33 +0900

    init

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>about</h1>
+        </div>
+    )
+}
+
+export default About
